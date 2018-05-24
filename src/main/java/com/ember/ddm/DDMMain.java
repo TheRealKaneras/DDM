82b01d407ee04e47d684675ae933b268d28e56ea
Adam Ali <kaneras42@gmail.com>
2018-05-24 20:52:18 +0100

    Just the start of the code... I mean, what more do you want from me now?

---
+++ src/main/java/com/ember/ddm/DDMMain.java
@@ -0,0 +1,36 @@
+package com.ember.ddm;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+import net.minecraftforge.fml.common.Mod.EventHandler;
+
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = DDMMain.MODID, name = DDMMain.NAME, version = DDMMain.VERSION)
+public class DDMMain {
+
+    public static final String MODID = "ddm";
+    public static final String NAME = "Disturbed Dimensions Mod";
+    public static final String VERSION = "prealpha 0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+} 
